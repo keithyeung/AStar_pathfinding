6f4d84d7ff936bb1913813379ff563aa04343348
keithyeung <77668033+keithyeung@users.noreply.github.com>
2023-06-29 23:31:23 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,11 +3,6 @@
 #include <ctime>
 #include<cstdlib>
 
-// IMPORTANT:
-// Feel free to not use the Level, and redo everything on your own. This is just some boilerplate code to get you started.
-
-// If you want, I suggest manually copying my code from scratch. That should help with understanding how it works a bit better.
-
 int main(void)
 {    
     srand(time(nullptr));
@@ -18,9 +13,6 @@ int main(void)
     
     SetConfigFlags(ConfigFlags::FLAG_WINDOW_RESIZABLE | ConfigFlags::FLAG_VSYNC_HINT | ConfigFlags::FLAG_MSAA_4X_HINT);
     InitWindow(screenWidth, screenHeight, "The Event Loop of Life");
-
-    // SetTargetFPS(60); // Uncomment this if you want to use fixed framerate (I suggest using vsync)
-    // SetExitKey(0);    // Uncomment this to disable ESC as exit key
 
     Level level;
 
